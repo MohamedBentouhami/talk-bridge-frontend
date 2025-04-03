1c30969e6fa60d0e0bce3358c3730225e81eda5f
Mohamed <mohamedbentouhami512@gmail.com>
2025-04-03 14:30:15 +0200

    add friends features

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -4,24 +4,11 @@ import { User } from "../@types/user";
 const userService = axios.create({
     baseURL: import.meta.env.VITE_API_URL,
     headers: {
-        "Authorization": localStorage.getItem("token")
+        "Authorization": localStorage.getItem("token") || ""
     }
-})
-
-export type UsersRequest = {
-    data: Array<{
-        id: string;
-        firstName: string;
-        lastName: string;
-    }>;
-};
-
+});
 
 export async function fetchNewPartners(lg: string): Promise<User[]> {
-    const response = await userService.get<any>(`/users/${lg}`);
-    return response.data.map((user: any) => ({
-        id: user.id,
-        firstName: user.firstName,
-        lastName: user.lastName
-    }));
+    const response = await userService.get<User[]>(`/users/${lg}`);
+    return response.data;
 }
